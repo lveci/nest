511cc3719a15b7d8f1f04b3d24784db7e3ab694e
lveci <lveci>
2008-04-07 22:37:02 +0000

    comments on field descriptions

--- nest-core/src/main/java/org/esa/nest/dataio/OrbitalDataRecordReader.java
+++ nest-core/src/main/java/org/esa/nest/dataio/OrbitalDataRecordReader.java
@@ -55,8 +55,11 @@ public class OrbitalDataRecordReader {
         if(in == null) return;
         
         try {
+            // Product specifier ('@ODR' or 'xODR').
             productSpecifier = readAn(4);
+            // Satellite name.
             satelliteName = readAn(8);
+            // Advised start of the data arc (UTC secs past 1.0 Jan 1985)
             arcStart = in.readInt();
         } catch (IOException e) {
 
@@ -68,10 +71,62 @@ public class OrbitalDataRecordReader {
         if(in == null) return;
 
         try {
+            // Length of the repeat cycle in 10^-3 days.
             lengthOfRepeatCycle = in.readInt();
+            // Arc number. This number refers to the orbital solution arcs as processed by DUT/DEOS.
             arcNumber = in.readInt();
+            // Number of data records following the two headers.
             numRecords = in.readInt();
+
+            // Version ID
+            //Field number 4 of the second header record may contain a positive number
+            //which refers to the list below.
+            //
+            // ID  Explanation
+            //-------------------------------------------------------------------------------
+            //  1  Orbits generated 'after the event'. Though generated with the operational
+            //     software, they do not contain predictions. The gravity model used is
+            //     GEM-T2.
+            //  2  As 1. The gravity model used is PGS4591.
+            //  3  Orbits generated with operational software in operational mode. Thus they
+            //     also contain predictions. The gravity model used is GEM-T2.
+            //  4  As 3. The gravity model used is PGS4591.
+            //  5. Orbits generated with enhanced operational software (Version 920110),
+            //     which is able to integrate over small orbital maneuvers. Thus predictions
+            //     are accurate in case the magnitude of the orbital maneuver was know in
+            //     advance. GEM-T2 model.
+            //  6. As 5. With the PGS4591 gravity model.
+            //  7. Orbits generated at 5 minute intervals. GEM-T2 model.
+            //  9. Orbits generated with enhanced operational software (Version 920110),
+            //     which is able to integrate over small orbital maneuvers.
+            //     These arcs do not contain predictions. The 5.5-day arcs cover data
+            //     until the end. GEM-T2 model.
+            // 10. As 9. The gravity model used is PGS4591.
+            //105. As 5. But with predictions removed. Arcs cut at 5.5 days.
+            //201. 5.5-day arcs. No predictions. JGM-1 gravity model. Geodyn II version 9208.
+            //     SLR-only solution.
+            //202. 5.5-day arcs. No predictions. JGM-1 gravity model. Geodyn II version 9208.
+            //     Solution based on SLR and altimeter tracking data.
+            //301. JGM-2 gravity model, improved station coordinates, new GM.
+            //     SLR-only solution.
+            //302. Operational. JGM-3 gravity model, SLR-only solution.
+            //303. as 301, altimeter (IGDR) xover data included as tracking data.
+            //322. Precise, JGM-3 gravity model, Quick-look SLR, OPR single satellite xovers
+            //304. JGM-3 gravity field, Quick-look SLR, OPR single satellite xovers
+            //     DUT(LSC)95L03 coordinate solution.
+            //305. as 404. Adds also OPR altimetric height above a seasonal mean sea
+            //             surface as tracking data. 6-hourly drag parameters.
+            //323. as 304.
+            //324. as 304. Includes also ERS-1/2 dual satellite xovers. 6-hourly drag
+            //             parameters.
+            //404. as 304. DGM-E04 gravity field model used instead of JGM-3
+            //405. as 305. DGM-E04 gravity field model used instead of JGM-3
+            //424. as 324. DGM-E04 gravity field model used instead of JGM-3
+            //504. as 304. EGM96 gravity field model used instead of JGM-3
+            //505. as 305. EGM96 gravity field model used instead of JGM-3
+            //524. as 324. EGM96 gravity field model used instead of JGM-3
             version = in.readInt();
+            
         } catch (IOException e) {
 
             System.out.print(e);
@@ -83,8 +138,17 @@ public class OrbitalDataRecordReader {
 
         try {
             data.time = in.readInt();
+            
+            //latitude
+            //@ODR: in microdegrees.
+            //xODR: in 0.1 microdegrees.
             data.latitude = in.readInt();
+
+            //longitude
+            //@ODR: in microdegrees, interval 0 to 360 degrees.
+	        //xODR: in 0.1 microdegrees, interval -180 to 180 degrees.
             data.longitude = in.readInt();
+
             data.heightOfCenterOfMass = in.readInt();
         } catch (IOException e) {
 
