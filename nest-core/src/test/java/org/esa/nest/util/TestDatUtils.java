fc6eea6683a0876bcc2d86e69eb0006a91e58b20
lveci <lveci>
2008-05-01 20:18:29 +0000

    *** empty log message ***

---
+++ nest-core/src/test/java/org/esa/nest/util/TestDatUtils.java
@@ -0,0 +1,35 @@
+package org.esa.nest.util;
+
+import junit.framework.TestCase;
+
+import java.util.Map;
+import java.util.Properties;
+import java.io.File;
+
+/**
+ * DatUtils Tester.
+ *
+ * @author lveci
+ */
+public class TestDatUtils extends TestCase {
+
+    public TestDatUtils(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testFindHomeFolder()
+    {
+        File file = DatUtils.findHomeFolder("\\config\\settings.xml");
+
+        assertTrue(file != null);
+        assertTrue(file.exists());
+    }
+}
