a37fe44712a558df8ab5f4d8c415425497a53aab
lveci <lveci>
2008-07-21 22:18:24 +0000

    *** empty log message ***

---
+++ nest-op-sar-tools/src/main/java/org/esa/nest/dat/MultilookOpAction.java
@@ -0,0 +1,25 @@
+package org.esa.nest.dat;
+
+import org.esa.beam.framework.ui.ModelessDialog;
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+
+/**
+ * ASAR Calibration action.
+ *
+ */
+public class MultilookOpAction extends AbstractVisatAction {
+
+    private ModelessDialog dialog;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new DefaultSingleTargetProductDialog("Multilook", getAppContext(), "Multilook", null);
+        }
+        dialog.show();
+
+    }
+}
