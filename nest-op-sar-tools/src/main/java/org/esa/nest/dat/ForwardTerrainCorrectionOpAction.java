250076860c1f0b1fc2c387e3e0d57587041705ae
lveci <lveci>
2008-07-24 22:30:56 +0000

    *** empty log message ***

---
+++ nest-op-sar-tools/src/main/java/org/esa/nest/dat/ForwardTerrainCorrectionOpAction.java
@@ -0,0 +1,31 @@
+package org.esa.nest.dat;
+
+import org.esa.beam.framework.ui.ModelessDialog;
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.beam.visat.VisatApp;
+
+/**
+ * Forward-Terrain-Correction action.
+ *
+ */
+public class ForwardTerrainCorrectionOpAction extends AbstractVisatAction {
+
+    private ModelessDialog dialog;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new DefaultSingleTargetProductDialog("Forward-Terrain-Correction", getAppContext(), "Forward-Terrain-Correction", null);
+        }
+        dialog.show();
+
+    }
+
+    @Override
+    public void updateState(final CommandEvent event) {
+        setEnabled(false);
+    }
+}
