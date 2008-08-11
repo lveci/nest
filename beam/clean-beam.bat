18993c0e86e70f8dc2518e8d25719a10a055d3d7
lveci <lveci>
2008-08-11 05:28:26 +0000

    *** empty log message ***

---
+++ beam/clean-beam.bat
@@ -0,0 +1,17 @@
+rmdir /S /Q beam-aatsr-sst
+rmdir /S /Q beam-binning
+rmdir /S /Q beam-chris-reader
+rmdir /S /Q beam-cluster-analysis
+rmdir /S /Q beam-collocation
+rmdir /S /Q beam-flhmci
+rmdir /S /Q beam-gpf-docgen
+rmdir /S /Q beam-help
+rmdir /S /Q beam-installer
+rmdir /S /Q beam-meris-cloud
+rmdir /S /Q beam-meris-ndvi
+rmdir /S /Q beam-meris-radtorefl
+rmdir /S /Q beam-meris-smac
+rmdir /S /Q beam-meris-smile
+rmdir /S /Q beam-pconvert
+rmdir /S /Q beam-unmix
+
