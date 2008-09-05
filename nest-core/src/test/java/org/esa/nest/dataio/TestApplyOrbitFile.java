1e5c500e537f57f3d8c124217918a56f43bb7246
lveci <lveci>
2008-09-05 18:52:06 +0000

    *** empty log message ***

---
+++ nest-core/src/test/java/org/esa/nest/dataio/TestApplyOrbitFile.java
@@ -0,0 +1,38 @@
+package org.esa.nest.dataio;
+
+import junit.framework.TestCase;
+import org.esa.beam.dataio.envisat.EnvisatOrbitReader;
+
+import java.io.File;
+import java.util.Date;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: lveci
+ * Date: Sep 4, 2008
+ * To change this template use File | Settings | File Templates.
+ */
+public class TestApplyOrbitFile  extends TestCase {
+
+    File vorPath = new File("P:\\nest\\nest\\ESA Data\\Orbits\\Doris\\vor");
+    
+    public TestApplyOrbitFile(String name) {
+        super(name);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    public void testOpenFile() {
+
+        EnvisatOrbitReader reader = new EnvisatOrbitReader();
+
+        //Date productDate = new Date(12345678);
+        //ApplyOrbitFile.FindOrbitFile(reader, vorPath, productDate);
+    }
+}
