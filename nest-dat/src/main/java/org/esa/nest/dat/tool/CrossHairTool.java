00ec0df13a30b410a286d453d9b00a46be0c18fb
lveci <lveci>
2008-09-24 18:40:07 +0000

    *** empty log message ***

--- nest-dat/src/main/java/org/esa/nest/dat/tool/CrossHairTool.java
+++ nest-dat/src/main/java/org/esa/nest/dat/tool/CrossHairTool.java
@@ -1,19 +1,4 @@
-/*
- * $Id: CrossHairTool.java,v 1.3 2008-09-09 22:09:23 lveci Exp $
- *
- * Copyright (C) 2002 by Brockmann Consult (info@brockmann-consult.de)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation. This program is distributed in the hope it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
+
 package org.esa.nest.dat.tool;
 
 import org.esa.beam.framework.ui.product.ProductSceneView;
--- nest-reader-ers-ceos/src/main/java/org/esa/nest/dataio/ceos/jers/JERSVolumeDirectoryFile.java
+++ nest-reader-ers-ceos/src/main/java/org/esa/nest/dataio/ceos/jers/JERSVolumeDirectoryFile.java
@@ -12,22 +12,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
-/*
- * $Id: JERSVolumeDirectoryFile.java,v 1.10 2008-09-19 16:51:03 lveci Exp $
- *
- * Copyright (C) 2002 by Brockmann Consult (info@brockmann-consult.de)
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation. This program is distributed in the hope it will
- * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
- * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- */
 
 /**
  * This class represents a volume directory file of JERS product.
