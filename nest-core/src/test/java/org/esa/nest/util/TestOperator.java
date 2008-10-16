f3ef914b180b1710a4a0e2f3c12d494aed51c57a
lveci <lveci>
2008-10-16 19:35:44 +0000

    *** empty log message ***

--- nest-core/src/test/java/org/esa/nest/util/TestOperator.java
+++ nest-core/src/test/java/org/esa/nest/util/TestOperator.java
@@ -1,11 +1,47 @@
 package org.esa.nest.util;
 
 import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.MetadataElement;
+import org.esa.beam.framework.datamodel.TiePointGrid;
+import org.esa.beam.framework.datamodel.TiePointGeoCoding;
+import org.esa.beam.framework.dataop.maptransf.Datum;
+import org.esa.nest.datamodel.AbstractMetadata;
 
 /**
  * Utilities for Operator unit tests
  */
 public class TestOperator {
+
+
+    public static Product createProduct(String type, int w, int h) {
+        Product product = new Product("name", type, w, h);
+
+        product.setStartTime(AbstractMetadata.parseUTC("10-MAY-2008 20:30:46.890683"));
+        product.setEndTime(AbstractMetadata.parseUTC("10-MAY-2008 20:35:46.890683"));
+        product.setDescription("description");
+
+        addGeoCoding(product);
+
+        AbstractMetadata.addAbstractedMetadataHeader(product.getMetadataRoot());
+
+        return product;
+    }
+
+    private static void addGeoCoding(final Product product) {
+
+        TiePointGrid latGrid = new TiePointGrid("lat", 2, 2, 0.5f, 0.5f,
+                product.getSceneRasterWidth(), product.getSceneRasterHeight(),
+                      new float[]{10.0f, 10.0f, 5.0f, 5.0f});
+        TiePointGrid lonGrid = new TiePointGrid("lon", 2, 2, 0.5f, 0.5f,
+                product.getSceneRasterWidth(), product.getSceneRasterHeight(),
+                      new float[]{10.0f, 10.0f, 5.0f, 5.0f},
+                      TiePointGrid.DISCONT_AT_360);
+        TiePointGeoCoding tpGeoCoding = new TiePointGeoCoding(latGrid, lonGrid, Datum.WGS_84);
+
+        product.addTiePointGrid(latGrid);
+        product.addTiePointGrid(lonGrid);
+        product.setGeoCoding(tpGeoCoding);
+    }
 
 
     public static void verifyProduct(Product product) throws Exception {
@@ -22,6 +58,20 @@ public class TestOperator {
         if(product.getStartTime() == null)
             throwErr("startTime is null");
 
+    }
+
+    public static void attributeEquals(MetadataElement elem, String name, double trueValue) throws Exception {
+        double val = elem.getAttributeDouble(name, 0);
+        if(Double.compare(val, trueValue) != 0) {
+            if(Float.compare((float)val, (float)trueValue) != 0)
+                throwErr(name + " is " + val + ", expecting " + trueValue);
+        }
+    }
+
+    public static void attributeEquals(MetadataElement elem, String name, String trueValue) throws Exception {
+        String val = elem.getAttributeString(name, "");
+        if(!val.equals(trueValue))
+            throwErr(name + " is " + val + ", expecting " + trueValue);
     }
 
     static void throwErr(String description) throws Exception {
--- nest-op-sar-tools/src/test/java/org/esa/nest/gpf/TestMultilookOperator.java
+++ nest-op-sar-tools/src/test/java/org/esa/nest/gpf/TestMultilookOperator.java
@@ -3,9 +3,9 @@ package org.esa.nest.gpf;
 import junit.framework.TestCase;
 import org.esa.beam.framework.gpf.OperatorSpi;
 import org.esa.beam.framework.gpf.GPF;
-import org.esa.beam.framework.gpf.Operator;
 import org.esa.beam.framework.datamodel.*;
 import org.esa.nest.datamodel.AbstractMetadata;
+import org.esa.nest.util.TestOperator;
 
 import java.util.Arrays;
 
@@ -31,6 +31,7 @@ public class TestMultilookOperator extends TestCase {
 
     /**
      * Tests multi-look operator with a 4x16 "DETECTED" test product.
+     * @throws Exception general exception
      */
     public void testMultilookOfRealImage() throws Exception {
 
@@ -42,7 +43,7 @@ public class TestMultilookOperator extends TestCase {
 
         // get targetProduct: execute initialize()
         Product targetProduct = op.getTargetProduct();
-        assertNotNull(targetProduct);
+        TestOperator.verifyProduct(targetProduct);
 
         Band band = targetProduct.getBandAt(0);
         assertNotNull(band);
@@ -56,31 +57,14 @@ public class TestMultilookOperator extends TestCase {
         assertTrue(Arrays.equals(expectedValues, floatValues));
 
         // compare updated metadata
-        MetadataElement abs = targetProduct.getMetadataRoot().getElement("Abstracted Metadata");
+        MetadataElement abs = targetProduct.getMetadataRoot().getElement(Product.ABSTRACTED_METADATA_ROOT_NAME);
 
-        MetadataAttribute azimuthLooksAttr = abs.getAttribute(AbstractMetadata.azimuth_looks);
-        float azimuth_looks = azimuthLooksAttr.getData().getElemFloat();
-        assertTrue(Float.compare(azimuth_looks, 2.0f) == 0);
-
-        MetadataAttribute rangeLooksAttr = abs.getAttribute(AbstractMetadata.range_looks);
-        float range_looks = rangeLooksAttr.getData().getElemFloat();
-        assertTrue(Float.compare(range_looks, 4.0f) == 0);
-
-        MetadataAttribute azimuthSpacingAttr = abs.getAttribute(AbstractMetadata.azimuth_spacing);
-        float azimuth_spacing = azimuthSpacingAttr.getData().getElemFloat();
-        assertTrue(Float.compare(azimuth_spacing, 4.0f) == 0);
-
-        MetadataAttribute rangeSpacingAttr = abs.getAttribute(AbstractMetadata.range_spacing);
-        float range_spacing = rangeSpacingAttr.getData().getElemFloat();
-        assertTrue(Float.compare(range_spacing, 2.0f) == 0);
-
-        MetadataAttribute lineTimeIntervalAttr = abs.getAttribute(AbstractMetadata.line_time_interval);
-        float lineTimeInterval = lineTimeIntervalAttr.getData().getElemFloat();
-        assertTrue(Float.compare(lineTimeInterval, 0.02f) == 0);
-
-        MetadataAttribute firstLineTimeAttr = abs.getAttribute(AbstractMetadata.first_line_time);
-        String firstLineTime = firstLineTimeAttr.getData().getElemString();
-        assertTrue(firstLineTime.equals("10-MAY-2008 20:32:46.890683"));
+        TestOperator.attributeEquals(abs, AbstractMetadata.azimuth_looks, 2.0);
+        TestOperator.attributeEquals(abs, AbstractMetadata.range_looks, 4.0);
+        TestOperator.attributeEquals(abs, AbstractMetadata.azimuth_spacing, 4.0);
+        TestOperator.attributeEquals(abs, AbstractMetadata.range_spacing, 2.0);
+        TestOperator.attributeEquals(abs, AbstractMetadata.line_time_interval, 0.02);
+        TestOperator.attributeEquals(abs, AbstractMetadata.first_line_time, "10-MAY-2008 20:32:46.890683");
     }
 
 
@@ -90,10 +74,13 @@ public class TestMultilookOperator extends TestCase {
      * 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32
      * 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48
      * 49 50 51 52 53 54 55 56 57 58 59 60 61 62 63 64
+     * @param w width
+     * @param h height
+     * @return the created product
      */
     private Product createTestProduct(int w, int h) {
 
-        Product testProduct = new Product("p", "ASA_APG_1P", w, h);
+        Product testProduct = TestOperator.createProduct("ASA_APG_1P", w, h);
 
         // create a Band: band1
         Band band1 = testProduct.addBand("band1", ProductData.TYPE_INT32);
@@ -106,40 +93,22 @@ public class TestMultilookOperator extends TestCase {
         band1.setData(ProductData.createInstance(intValues));
 
         // create abstracted metadata
-        MetadataElement abs = new MetadataElement("Abstracted Metadata");
+        MetadataElement abs = testProduct.getMetadataRoot().getElement(Product.ABSTRACTED_METADATA_ROOT_NAME);
 
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.SAMPLE_TYPE,
-                ProductData.createInstance("DETECTED"), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.MISSION,
-                ProductData.createInstance("ENVISAT"), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.srgr_flag,
-                ProductData.createInstance(new byte[] {0}), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.range_spacing,
-                ProductData.createInstance(new float[] {0.5F}), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.azimuth_spacing,
-                ProductData.createInstance(new float[] {2.0F}), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.azimuth_looks,
-                ProductData.createInstance(new int[] {1}), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.range_looks,
-                ProductData.createInstance(new int[] {1}), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.line_time_interval,
-                ProductData.createInstance(new float[] {0.01F}), false));
-
-        abs.addAttribute(new MetadataAttribute(AbstractMetadata.first_line_time,
-                ProductData.createInstance("10-MAY-2008 20:32:46.885684"), false));
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.SAMPLE_TYPE, "DETECTED");
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.MISSION, "ENVISAT");
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.srgr_flag, 0);
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.range_spacing, 0.5F);
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.azimuth_spacing, 2.0F);
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.azimuth_looks, 1);
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.range_looks, 1);
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.line_time_interval, 0.01F);
+        AbstractMetadata.setAttribute(abs, AbstractMetadata.first_line_time,
+                AbstractMetadata.parseUTC("10-MAY-2008 20:32:46.885684"));
 
         float[] incidence_angle = new float[64];
         Arrays.fill(incidence_angle, 30.0f);
         testProduct.addTiePointGrid(new TiePointGrid("incident_angle", 16, 4, 0, 0, 1, 1, incidence_angle));
-
-        testProduct.getMetadataRoot().addElement(abs);
 
         return testProduct;
     }
