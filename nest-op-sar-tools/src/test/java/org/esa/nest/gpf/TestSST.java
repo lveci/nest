0b552c1a4479e906d127b3f4726faae9c81acd97
lveci <lveci>
2008-11-24 19:02:20 +0000

    *** empty log message ***

---
+++ nest-op-sar-tools/src/test/java/org/esa/nest/gpf/TestSST.java
@@ -0,0 +1,61 @@
+package org.esa.nest.gpf;
+
+import junit.framework.TestCase;
+
+/*import shared.array.AbstractArray;
+import shared.array.ComplexArray;
+import shared.fft.ConvolutionCache;
+import sharedx.fftw.Plan;*/
+
+public class TestSST extends TestCase {
+
+    @Override
+    protected void setUp() throws Exception {
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+    }
+
+    public void testSST() {
+
+    }
+
+
+    final public static void initClass() {
+
+    /*            final String modeStr;
+
+                switch (MODE) {
+
+                case Plan.FFTW_ESTIMATE:
+                        modeStr = "estimate";
+                        break;
+
+                case Plan.FFTW_MEASURE:
+                        modeStr = "measure";
+                        break;
+
+                case Plan.FFTW_PATIENT:
+                        modeStr = "patient";
+                        break;
+
+                case Plan.FFTW_EXHAUSTIVE:
+                        modeStr = "exhaustive";
+                        break;
+
+                default:
+                        throw new AssertionError();
+                }
+
+                AbstractArray.FFTService.setHint("mode", modeStr);
+
+                ComplexArray tmp = new ComplexArray(SIZE, SIZE, 2);
+
+                for (int i = 0; i < 64; i++) {
+                        tmp.fft().ifft();
+                }
+
+                */
+        }
+}
