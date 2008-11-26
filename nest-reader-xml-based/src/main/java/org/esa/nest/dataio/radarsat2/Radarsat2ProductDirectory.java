b7c730b3d67adb667fb674a6728b731fa8d892e2
lveci <lveci>
2008-11-26 22:27:11 +0000

    *** empty log message ***

--- nest-reader-xml-based/src/main/java/org/esa/nest/dataio/radarsat2/Radarsat2ProductDirectory.java
+++ nest-reader-xml-based/src/main/java/org/esa/nest/dataio/radarsat2/Radarsat2ProductDirectory.java
@@ -1,11 +1,14 @@
 package org.esa.nest.dataio.radarsat2;
 
-import org.esa.beam.framework.datamodel.MetadataElement;
-import org.esa.beam.framework.datamodel.Product;
+import org.esa.beam.framework.datamodel.*;
+import org.esa.beam.framework.dataop.maptransf.Datum;
+import org.esa.beam.util.Guardian;
 import org.esa.nest.dataio.XMLProductDirectory;
 import org.esa.nest.datamodel.AbstractMetadata;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.ArrayList;
 
 /**
  * This class represents a product directory.
@@ -22,49 +25,172 @@ public class Radarsat2ProductDirectory extends XMLProductDirectory {
     }
 
     @Override
-    protected void addGeoCoding(final Product product) {
-
-   /*     TiePointGrid latGrid = new TiePointGrid("lat", 2, 2, 0.5f, 0.5f,
-                product.getSceneRasterWidth(), product.getSceneRasterHeight(),
-                                                _leaderFile.getLatCorners());
-        TiePointGrid lonGrid = new TiePointGrid("lon", 2, 2, 0.5f, 0.5f,
-                product.getSceneRasterWidth(), product.getSceneRasterHeight(),
-                                                _leaderFile.getLonCorners(),
-                                                TiePointGrid.DISCONT_AT_360);
-        TiePointGeoCoding tpGeoCoding = new TiePointGeoCoding(latGrid, lonGrid, Datum.WGS_84);
-
-        product.addTiePointGrid(latGrid);
-        product.addTiePointGrid(lonGrid);
-        product.setGeoCoding(tpGeoCoding);  */
-    }
-
-    @Override
     protected void addAbstractedMetadataHeader(Product product, MetadataElement root) {
 
         AbstractMetadata.addAbstractedMetadataHeader(root);
 
+        final String defStr = AbstractMetadata.NO_METADATA_STRING;
+        final int defInt = AbstractMetadata.NO_METADATA;
+
         final MetadataElement absRoot = root.getElement(Product.ABSTRACTED_METADATA_ROOT_NAME);
+        final MetadataElement productElem = root.getElement("product");
 
-        //mph
+        // sourceAttributes
+        final MetadataElement sourceAttributes = productElem.getElement("sourceAttributes");
+
         AbstractMetadata.setAttribute(absRoot, AbstractMetadata.PRODUCT, productName);
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.MISSION, "RADARSAT-2");
+
+        final MetadataElement radarParameters = sourceAttributes.getElement("radarParameters");
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.SPH_DESCRIPTOR,
+                radarParameters.getAttributeString("acquisitionType", " "));
+        
+        final MetadataElement pulseRepetitionFrequency = radarParameters.getElement("pulseRepetitionFrequency");
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.pulse_repetition_frequency,
+                pulseRepetitionFrequency.getAttributeDouble("pulseRepetitionFrequency", 0));
+
+        final MetadataElement orbitAndAttitude = sourceAttributes.getElement("orbitAndAttitude");
+        final MetadataElement orbitInformation = orbitAndAttitude.getElement("orbitInformation");
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.PASS,
+                orbitInformation.getAttributeString("passDirection", " ").toUpperCase());
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.orbit_state_vector_file,
+                orbitInformation.getAttributeString("orbitDataFile", " "));
+
+        // imageGenerationParameters
+        final MetadataElement imageGenerationParameters = productElem.getElement("imageGenerationParameters");
+        final MetadataElement generalProcessingInformation = imageGenerationParameters.getElement("generalProcessingInformation");
+
+        productType = generalProcessingInformation.getAttributeString("productType", " ");
         AbstractMetadata.setAttribute(absRoot, AbstractMetadata.PRODUCT_TYPE, productType);
-       /* AbstractMetadata.setAttribute(absRoot, AbstractMetadata.SPH_DESCRIPTOR,
-                _leaderFile.getSceneRecord().getAttributeString("Product type descriptor"));
-        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.MISSION, getMission());
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.PROC_TIME, getTime(generalProcessingInformation, "processingTime"));
 
-        String procTime = _volumeDirectoryFile.getTextRecord().getAttributeString("Location and datetime of product creation").trim();
-        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.PROC_TIME, procTime );
-        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.REL_ORBIT,
-                Integer.parseInt(_leaderFile.getSceneRecord().getAttributeString("Orbit number").trim()));
-        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.ABS_ORBIT,
-                Integer.parseInt(_leaderFile.getSceneRecord().getAttributeString("Orbit number").trim()));
-        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.STATE_VECTOR_TIME,
-                _leaderFile.getFacilityRecord().getAttributeString("Time of input state vector used to processed the image"));
+        final MetadataElement sarProcessingInformation = imageGenerationParameters.getElement("sarProcessingInformation");
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.ant_elev_corr_flag,
+                getFlag(sarProcessingInformation, "elevationPatternCorrection"));
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.range_spread_comp_flag,
+                getFlag(sarProcessingInformation, "rangeSpreadingLossCorrection"));
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.abs_calibration_flag, 
+                getFlag(sarProcessingInformation, "rawDataCorrection"));
+
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.first_line_time,
+                getTime(sarProcessingInformation, "zeroDopplerTimeFirstLine"));
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.last_line_time,
+                getTime(sarProcessingInformation, "zeroDopplerTimeLastLine"));
+
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.range_looks,
+                sarProcessingInformation.getAttributeInt("numberOfRangeLooks", 0));
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.azimuth_looks,
+                sarProcessingInformation.getAttributeInt("numberOfAzimuthLooks", 0));
+
+        // imageAttributes
+        final MetadataElement imageAttributes = productElem.getElement("imageAttributes");
+        final MetadataElement rasterAttributes = imageAttributes.getElement("rasterAttributes");
+
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.SAMPLE_TYPE,
+                rasterAttributes.getAttributeString("dataType", defStr).toUpperCase());
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.num_output_lines,
+                rasterAttributes.getAttributeInt("numberOfLines", defInt));
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.num_samples_per_line,
+                rasterAttributes.getAttributeInt("numberOfSamplesPerLine", defInt));
+
+        final MetadataElement sampledPixelSpacing = rasterAttributes.getElement("sampledPixelSpacing");
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.range_spacing,
+                sampledPixelSpacing.getAttributeDouble("sampledPixelSpacing", defInt));
+        final MetadataElement sampledLineSpacing = rasterAttributes.getElement("sampledLineSpacing");
+        AbstractMetadata.setAttribute(absRoot, AbstractMetadata.azimuth_spacing,
+                sampledLineSpacing.getAttributeDouble("sampledLineSpacing", defInt));
 
 
-        //sph
 
-        AbstractMetadata.setAttribute(absRoot, "SAMPLE_TYPE", getSampleType());    */
+
+        
+    }
+
+    private static int getFlag(MetadataElement elem, String tag) {
+        String valStr = elem.getAttributeString(tag, " ").toUpperCase();
+        if(valStr.equals("FALSE") || valStr.equals("0"))
+            return 0;
+        else if(valStr.equals("TRUE") || valStr.equals("1"))
+            return 1;
+        return -1;
+    }
+
+    private static ProductData.UTC getTime(MetadataElement elem, String tag) {
+        final String timeStr = createValidUTCString(elem.getAttributeString(tag, " ").toUpperCase(),
+                new char[]{':','.','-'}, ' ').trim();
+        return AbstractMetadata.parseUTC(timeStr, "yyyy-mm-dd HH:mm:ss");
+    }
+
+    private static String createValidUTCString(String name, char[] validChars, char replaceChar) {
+        Guardian.assertNotNull("name", name);
+        char[] sortedValidChars = null;
+        if (validChars == null) {
+            sortedValidChars = new char[0];
+        } else {
+            sortedValidChars = (char[]) validChars.clone();
+        }
+        Arrays.sort(sortedValidChars);
+        StringBuilder validName = new StringBuilder(name.length());
+        for (int i = 0; i < name.length(); i++) {
+            final char ch = name.charAt(i);
+            if (Character.isDigit(ch)) {
+                validName.append(ch);
+            } else if (Arrays.binarySearch(sortedValidChars, ch) >= 0) {
+                validName.append(ch);
+            } else {
+                validName.append(replaceChar);
+            }
+        }
+        return validName.toString();
+    }
+
+    @Override
+    protected void addGeoCoding(final Product product) {
+
+        final MetadataElement root = product.getMetadataRoot();
+        final MetadataElement productElem = root.getElement("product");
+        final MetadataElement imageAttributes = productElem.getElement("imageAttributes");
+        final MetadataElement geographicInformation = imageAttributes.getElement("geographicInformation");
+        final MetadataElement geolocationGrid = geographicInformation.getElement("geolocationGrid");
+
+        MetadataElement[] geoGrid = geolocationGrid.getElements();
+
+        float[] latList = new float[geoGrid.length];
+        float[] lngList = new float[geoGrid.length];
+        int gridWidth = 0, gridHeight = 0;
+        int i=0;
+        for(MetadataElement imageTiePoint : geoGrid) {
+            final MetadataElement geodeticCoordinate = imageTiePoint.getElement("geodeticCoordinate");
+            final MetadataElement latitude = geodeticCoordinate.getElement("latitude");
+            final MetadataElement longitude = geodeticCoordinate.getElement("longitude");
+            latList[i] = (float)latitude.getAttributeDouble("latitude", 0);
+            lngList[i] = (float)longitude.getAttributeDouble("longitude", 0);
+
+            final MetadataElement imageCoordinate = imageTiePoint.getElement("imageCoordinate");
+            final double pix = imageCoordinate.getAttributeDouble("pixel", 0);
+            if(pix == 0) {
+                if(gridWidth == 0)
+                    gridWidth = i;
+                ++gridHeight;
+            }
+
+            ++i;
+        }
+
+        float subSamplingX = (float)product.getSceneRasterWidth() / (gridWidth - 1);
+        float subSamplingY = (float)product.getSceneRasterHeight() / (gridHeight - 1);
+
+        final TiePointGrid latGrid = new TiePointGrid("lat", gridWidth, gridHeight, 0.5f, 0.5f,
+                subSamplingX, subSamplingY, latList);
+
+        final TiePointGrid lonGrid = new TiePointGrid("lon", gridWidth, gridHeight, 0.5f, 0.5f,
+                subSamplingX, subSamplingY, lngList, TiePointGrid.DISCONT_AT_180);
+        
+        TiePointGeoCoding tpGeoCoding = new TiePointGeoCoding(latGrid, lonGrid, Datum.WGS_84);
+
+        product.addTiePointGrid(latGrid);
+        product.addTiePointGrid(lonGrid);
+        product.setGeoCoding(tpGeoCoding);    
     }
 
     @Override
