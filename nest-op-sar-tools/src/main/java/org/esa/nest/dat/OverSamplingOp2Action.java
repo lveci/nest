c6ca7a03ad69536e17996b7c417591994946314f
junlu <junlu>
2008-12-12 01:26:43 +0000

    action for OversamplingOp2

---
+++ nest-op-sar-tools/src/main/java/org/esa/nest/dat/OverSamplingOp2Action.java
@@ -0,0 +1,27 @@
+package org.esa.nest.dat;
+
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.nest.dat.dialogs.NestSingleTargetProductDialog;
+
+/**
+ * OverSamplingOp action.
+ *
+ */
+public class OverSamplingOp2Action extends AbstractVisatAction {
+
+    private NestSingleTargetProductDialog dialog;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new NestSingleTargetProductDialog("Oversample2", getAppContext(), "Oversample2", getHelpId());
+            dialog.setTargetProductNameSuffix("_OvrS");
+        }
+        dialog.show();
+
+    }
+
+}
