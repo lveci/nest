43175665a1ddb192ada6f061c515b2e338bbfc50
lveci <lveci>
2009-03-03 22:25:46 +0000

    *** empty log message ***

--- nest-op-coregistration/src/main/java/org/esa/nest/dat/PCAAction.java
+++ nest-op-coregistration/src/main/java/org/esa/nest/dat/PCAAction.java
@@ -7,7 +7,7 @@ public class PCAAction extends AbstractVisatAction {
 
     @Override
     public void actionPerformed(final CommandEvent event) {
-        PCADialog dialog = new PCADialog(getAppContext(), "Principle Component Analysis", "PCA");
+        PCADialog dialog = new PCADialog(getAppContext(), "Principle Component Analysis", "PCAOp");
         dialog.setTargetProductNameSuffix("_PCA");
         dialog.show();
     }
