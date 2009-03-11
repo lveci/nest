5e92c705865125cacbc8192b508b9e2401eb2c17
junlu <junlu>
2009-03-11 18:39:28 +0000

    modified

--- nest-op-orthorectification/src/main/java/org/esa/nest/dat/ForwardTerrainCorrectionOpAction.java
+++ nest-op-orthorectification/src/main/java/org/esa/nest/dat/ForwardTerrainCorrectionOpAction.java
@@ -4,6 +4,7 @@ import org.esa.beam.framework.ui.ModelessDialog;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
 import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.nest.dat.dialogs.NestSingleTargetProductDialog;
 
 /**
  * Forward-Terrain-Correction action.
@@ -11,14 +12,15 @@ import org.esa.beam.visat.actions.AbstractVisatAction;
  */
 public class ForwardTerrainCorrectionOpAction extends AbstractVisatAction {
 
-    private ModelessDialog dialog;
+    private NestSingleTargetProductDialog dialog;
 
     @Override
     public void actionPerformed(CommandEvent event) {
 
         if (dialog == null) {
-            dialog = new DefaultSingleTargetProductDialog("Forward-Terrain-Correction", getAppContext(),
+            dialog = new NestSingleTargetProductDialog("Forward-Terrain-Correction", getAppContext(),
                     "Forward-Terrain-Correction", getHelpId());
+            dialog.setTargetProductNameSuffix("_OrthRect");
         }
         dialog.show();
 
