202d62c4b11c038565207ff8d0d31db621f8fc5f
junlu <junlu>
2009-03-26 20:31:49 +0000

    added semi-major/minor axis of earth

--- nest-core/src/main/java/org/esa/nest/util/Constants.java
+++ nest-core/src/main/java/org/esa/nest/util/Constants.java
@@ -5,6 +5,9 @@ public final class Constants
     public static final double lightSpeed = 299792458.0; //  m / s
     public static final double halfLightSpeed = lightSpeed / 2.0;
 
+    public static final double semiMajorAxis = 6378137.0;      // in m, WGS84 semi-major axis of Earth
+    public static final double semiMinorAxis = 6356752.314245; // in m, WGS84 semi-minor axis of Earth
+
     public static final double oneMillion = 1000000.0;
 
     private Constants()
