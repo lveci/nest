a31c8c516db673eb6be1887379bebbdc5ff0ac8b
junlu <junlu>
2009-04-07 19:42:16 +0000

    range doppler geocoding operator action

---
+++ nest-op-orthorectification/src/main/java/org/esa/nest/dat/RangeDopplerGeocodingOpAction.java
@@ -0,0 +1,26 @@
+package org.esa.nest.dat;
+
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.nest.dat.dialogs.NestSingleTargetProductDialog;
+
+/**
+ * Range-Doppler-Geocoding action.
+ *
+ */
+public class RangeDopplerGeocodingOpAction extends AbstractVisatAction {
+
+    private NestSingleTargetProductDialog dialog = null;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new NestSingleTargetProductDialog(
+                    "Range-Doppler-Geocoding", getAppContext(), "Range-Doppler-Geocoding", getHelpId());
+            dialog.setTargetProductNameSuffix("_GEOCODED");
+        }
+        dialog.show();
+
+    }
+}
