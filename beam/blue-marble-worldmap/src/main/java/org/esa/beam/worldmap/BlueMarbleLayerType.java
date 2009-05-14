092478ee467893ff4feaf4b7589bd3ae1367bdb1
lveci <lveci>
2009-05-14 22:33:01 +0000

    *** empty log message ***

---
+++ beam/blue-marble-worldmap/src/main/java/org/esa/beam/worldmap/BlueMarbleLayerType.java
@@ -0,0 +1,41 @@
+package org.esa.beam.worldmap;
+
+import com.bc.ceres.binding.ValueContainer;
+import com.bc.ceres.glayer.Layer;
+import com.bc.ceres.glayer.LayerContext;
+import com.bc.ceres.glayer.LayerType;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.opengis.referencing.crs.CoordinateReferenceSystem;
+
+/**
+ * @author Marco Peters
+ * @version $Revision: 1.1 $ $Date: 2009-05-14 18:33:01 $
+ * @since BEAM 4.6
+ */
+public class BlueMarbleLayerType extends LayerType {
+
+    @Override
+    public String getName() {
+        return "Nasa Blue Marble";
+    }
+
+    @Override
+    public boolean isValidFor(LayerContext ctx) {
+        if (ctx.getCoordinateReferenceSystem() instanceof CoordinateReferenceSystem) {
+            CoordinateReferenceSystem crs = (CoordinateReferenceSystem) ctx.getCoordinateReferenceSystem();
+            return DefaultGeographicCRS.WGS84.equals(crs);
+        }
+        return false;
+    }
+
+    @Override
+    protected Layer createLayerImpl(LayerContext ctx, ValueContainer configuration) {
+        return new BlueMarbleWorldMapLayer(configuration);
+    }
+
+    @Override
+    public ValueContainer getConfigurationTemplate() {
+        return new ValueContainer();
+
+    }
+}
