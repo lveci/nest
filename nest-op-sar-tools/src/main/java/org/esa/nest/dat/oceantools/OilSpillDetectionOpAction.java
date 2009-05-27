9ab011b9a6e21f78df90f1d3312aed79d21dff79
lveci <lveci>
2009-05-27 18:35:04 +0000

    *** empty log message ***

---
+++ nest-op-sar-tools/src/main/java/org/esa/nest/dat/oceantools/OilSpillDetectionOpAction.java
@@ -0,0 +1,31 @@
+package org.esa.nest.dat.oceantools;
+
+import org.esa.beam.framework.ui.ModelessDialog;
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.beam.visat.VisatApp;
+
+/**
+ * Oil Spill Detection action.
+ *
+ */
+public class OilSpillDetectionOpAction extends AbstractVisatAction {
+
+    private ModelessDialog dialog;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new DefaultSingleTargetProductDialog("OilSpillDetectionOp", getAppContext(), "OilSpillDetectionOp", getHelpId());
+        }
+        dialog.show();
+
+    }
+
+    @Override
+    public void updateState(final CommandEvent event) {
+        setEnabled(false);
+    }
+}
---
+++ nest-op-sar-tools/src/main/java/org/esa/nest/dat/oceantools/WindFieldEstimationOpAction.java
@@ -0,0 +1,31 @@
+package org.esa.nest.dat.oceantools;
+
+import org.esa.beam.framework.ui.ModelessDialog;
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+import org.esa.beam.visat.VisatApp;
+
+/**
+ * Wind Field Estimation action.
+ *
+ */
+public class WindFieldEstimationOpAction extends AbstractVisatAction {
+
+    private ModelessDialog dialog;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new DefaultSingleTargetProductDialog("Wind-Field-Estimation ", getAppContext(), "Wind Field Estimation ", getHelpId());
+        }
+        dialog.show();
+
+    }
+
+    @Override
+    public void updateState(final CommandEvent event) {
+        setEnabled(false);
+    }
+}
