388655580c580c8d5282739e9526c167bfc47b2b
junlu <junlu>
2009-08-28 18:16:45 +0000

    action for discrimination operator

---
+++ nest-op-sar-tools/src/main/java/org/esa/nest/dat/oceantools/ObjectTrackerDiscriminationOpAction.java
@@ -0,0 +1,30 @@
+package org.esa.nest.dat.oceantools;
+
+import org.esa.beam.framework.gpf.ui.DefaultSingleTargetProductDialog;
+import org.esa.beam.framework.ui.ModelessDialog;
+import org.esa.beam.framework.ui.command.CommandEvent;
+import org.esa.beam.visat.actions.AbstractVisatAction;
+
+/**
+ * Object Tracker Discrimination action.
+ *
+ */
+public class ObjectTrackerDiscriminationOpAction extends AbstractVisatAction {
+
+    private ModelessDialog dialog;
+
+    @Override
+    public void actionPerformed(CommandEvent event) {
+
+        if (dialog == null) {
+            dialog = new DefaultSingleTargetProductDialog("Object-Tracker-Discrimination", getAppContext(), "Object-Tracker-Discrimination", getHelpId());
+        }
+        dialog.show();
+
+    }
+
+    @Override
+    public void updateState(final CommandEvent event) {
+        setEnabled(true);
+    }
+}
