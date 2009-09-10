4fc6c6f191411ebbf2b2c0caa1fcbe4eb3e320c4
junlu <junlu>
2009-09-10 01:57:13 +0000

    added test for refined Lee filter

--- nest-op-sar-tools/src/test/java/org/esa/nest/gpf/filtering/SpeckleFilterOperatorTest.java
+++ nest-op-sar-tools/src/test/java/org/esa/nest/gpf/filtering/SpeckleFilterOperatorTest.java
@@ -183,6 +183,43 @@ public class SpeckleFilterOperatorTest extends TestCase {
     }
 
     /**
+     * Tests refined Lee speckle filter with a 7-by-7 test product.
+     * @throws Exception anything
+     */
+    public void testRefinedLeeFilter() throws Exception {
+        final Product sourceProduct = createRefinedLeeTestProduct();
+
+        final SpeckleFilterOp op = (SpeckleFilterOp)spi.createOperator();
+        assertNotNull(op);
+        op.setSourceProduct(sourceProduct);
+        op.SetFilter("Refined Lee");
+
+        // get targetProduct gets initialize to be executed
+        final Product targetProduct = op.getTargetProduct();
+        TestUtils.verifyProduct(targetProduct, true, true);
+
+        final Band band = targetProduct.getBandAt(0);
+        assertNotNull(band);
+
+        // readPixels gets computeTiles to be executed
+        final float[] floatValues = new float[49];
+        band.readPixels(0, 0, 7, 7, floatValues, ProgressMonitor.NULL);
+
+        // compare with expected outputs
+        final float[] expectedValues = {
+                109.1105f, 110.5941f, 117.78618f, 128.18275f, 119.57421f, 47.09596f, 71.89752f,
+                112.016556f, 103.36993f, 132.47015f, 98.507126f, 110.91507f, 68.164635f, 58.384193f,
+                111.90301f, 102.11465f, 122.47066f, 132.57114f, 95.87851f, 64.76915f, 82.97935f,
+                128.10281f, 125.521416f, 128.53871f, 110.785545f, 99.29122f, 58.20216f, 85.11038f,
+                132.0234f, 134.37022f, 111.09297f, 127.21618f, 114.163055f, 46.319008f, 64.66793f,
+                111.04274f, 102.463974f, 103.09271f, 109.065636f, 101.80986f, 39.772293f, 71.14607f,
+                114.20321f, 135.49301f, 113.130165f, 119.92864f, 100.15053f, 78.08161f, 71.565735f
+        };
+
+        assertTrue(Arrays.equals(expectedValues, floatValues));
+    }
+
+    /**
      * Creates a 4-by-4 test product as shown below for speckle filter tests:
      *  1  2  3  4
      *  5  6  7  8
@@ -200,6 +237,25 @@ public class SpeckleFilterOperatorTest extends TestCase {
         for (int i = 0; i < w * h; i++) {
             intValues[i] = i + 1;
         }
+        band1.setData(ProductData.createInstance(intValues));
+        band1.setUnit(Unit.AMPLITUDE);
+        return testProduct;
+    }
+
+    private static Product createRefinedLeeTestProduct() {
+        int w = 7;
+        int h = 7;
+        final Product testProduct = TestUtils.createProduct("type", w, h);
+        final Band band1 = testProduct.addBand("band1", ProductData.TYPE_INT32);
+        band1.setSynthetic(true);
+        final int[] intValues = { 99, 105, 124, 138, 128, 34, 62,
+                                 105,  91, 140,  98, 114, 63, 31,
+                                 107,  94, 128, 138,  96, 61, 82,
+                                 137, 129, 136, 105, 100, 55, 85,
+                                 144, 145, 113, 132, 119, 39, 50,
+                                 102,  97, 102, 110, 103, 34, 53,
+                                 107, 146, 115, 123, 101, 76, 56};
+
         band1.setData(ProductData.createInstance(intValues));
         band1.setUnit(Unit.AMPLITUDE);
         return testProduct;
