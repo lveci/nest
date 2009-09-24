abd6246bd79cc087589dcc00f92bacabf7c7b90a
lveci <lveci>
2009-09-24 00:53:01 +0000

    fix archetypes

---
+++ nest-developer-tools/create-gpf-op.bat
@@ -0,0 +1,7 @@
+mvn archetype:generate ^
+ -DarchetypeGroupId=org.esa.nest.maven ^
+ -DarchetypeArtifactId=maven-nest-gpf-archetype ^
+ -DarchetypeVersion=3B ^
+ -DgroupId=org.esa.nest ^
+ -DartifactId=%1 ^
+ -Dversion=3B
--- nest-developer-tools/maven-nest-gpf-archetype/create-gpf-op.bat
+++
@@ -1,8 +0,0 @@
-mvn archetype:create  ^
- -DarchetypeGroupId=org.esa.beam.maven ^
- -DarchetypeArtifactId=maven-beam-gpf-archetype ^
- -DarchetypeVersion=1.0 ^
- -DgroupId=%1 ^
- -DartifactId=%2 ^
- -Dversion=1.0-SNAPSHOT ^
- -DpackageName=%1
