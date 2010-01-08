dd5d3e09ee1a1b35535f93927fb6f883f0144d25
lveci <lveci>
2010-01-08 19:50:32 +0000

    *** empty log message ***

--- nest-core/src/test/java/org/esa/nest/util/TestDatUtils.java
+++ nest-core/src/test/java/org/esa/nest/util/TestDatUtils.java
@@ -25,7 +25,7 @@ public class TestDatUtils extends TestCase {
 
     public void testFindHomeFolder()
     {
-        File file = ResourceUtils.findInHomeFolder(File.separator + "config" + File.separator + "settings.xml");
+        final File file = ResourceUtils.findInHomeFolder(File.separator + "config" + File.separator + "settings_win.xml");
 
         assertTrue(file != null);
         assertTrue(file.exists());
