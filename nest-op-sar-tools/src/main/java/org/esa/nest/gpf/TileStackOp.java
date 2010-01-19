3c677ddb0231e00f922b771b211d72ce5055b8af
lveci <lveci>
2010-01-19 20:35:35 +0000

    *** empty log message ***

--- nest-op-sar-tools/src/main/java/org/esa/nest/gpf/TileStackOp.java
+++ nest-op-sar-tools/src/main/java/org/esa/nest/gpf/TileStackOp.java
@@ -21,7 +21,7 @@ import java.util.Map;
 
 /**
  */
-@OperatorMetadata(alias="TileStackOp", internal=false)
+@OperatorMetadata(alias="TileStackOp", internal=true)
 public class TileStackOp extends Operator {
 
     @SourceProduct(alias="source")
