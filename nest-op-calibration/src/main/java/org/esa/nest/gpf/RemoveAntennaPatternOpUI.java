1721e4016126ba2acf7c332f9f8b9250f0bc7065
junlu <junlu>
2010-01-20 23:30:45 +0000

    ui for removeAntennaPatternOp

---
+++ nest-op-calibration/src/main/java/org/esa/nest/gpf/RemoveAntennaPatternOpUI.java
@@ -0,0 +1,82 @@
+package org.esa.nest.gpf;
+
+import org.esa.beam.framework.gpf.ui.BaseOperatorUI;
+import org.esa.beam.framework.gpf.ui.UIValidation;
+import org.esa.beam.framework.ui.AppContext;
+import org.esa.beam.framework.ui.GridBagUtils;
+import org.esa.nest.util.DialogUtils;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+import java.util.Map;
+
+/**
+ * User interface for RemoveAntennaPatternOp
+ */
+public class RemoveAntennaPatternOpUI extends BaseOperatorUI {
+
+    private final JList bandList = new JList();
+
+    @Override
+    public JComponent CreateOpTab(String operatorName, Map<String, Object> parameterMap, AppContext appContext) {
+
+        initializeOperatorUI(operatorName, parameterMap);
+        final JComponent panel = createPanel();
+        initParameters();
+
+        return new JScrollPane(panel);
+    }
+
+    @Override
+    public void initParameters() {
+        OperatorUIUtils.initBandList(bandList, getBandNames());
+    }
+
+    @Override
+    public UIValidation validateParameters() {
+
+        return new UIValidation(UIValidation.State.OK, "");
+    }
+
+    @Override
+    public void updateParameters() {
+        OperatorUIUtils.updateBandList(bandList, paramMap);
+    }
+
+    private JComponent createPanel() {
+
+        final JPanel contentPane = new JPanel();
+        contentPane.setLayout(new GridBagLayout());
+        final GridBagConstraints gbc = GridBagUtils.createDefaultConstraints();
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.anchor = GridBagConstraints.NORTHWEST;
+        gbc.insets.top = 1;
+        gbc.insets.bottom = 1;
+        gbc.insets.right = 1;
+        gbc.insets.left = 1;
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        contentPane.add(new JLabel("Source Bands:"), gbc);
+
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = 1;
+        contentPane.add(new JScrollPane(bandList), gbc);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.gridx = 0;
+        gbc.gridy++;
+        DialogUtils.addComponent(contentPane, gbc, "Note:",
+                new JLabel("This operator removes antenna pattern and range spreading loss "));
+        gbc.gridy++;
+        DialogUtils.addComponent(contentPane, gbc, "",
+                new JLabel("for ASAR and ERS products. It also applies ADC to ERS product."));
+
+        DialogUtils.fillPanel(contentPane, gbc);
+
+        return contentPane;
+    }
+}
