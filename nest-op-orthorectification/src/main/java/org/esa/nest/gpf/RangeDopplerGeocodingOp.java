f00c54652ccd9e206088ec807c89a7c3b7742d0a
junlu <junlu>
2010-01-21 00:52:58 +0000

    rephrased error message

--- nest-op-orthorectification/src/main/java/org/esa/nest/gpf/RangeDopplerGeocodingOp.java
+++ nest-op-orthorectification/src/main/java/org/esa/nest/gpf/RangeDopplerGeocodingOp.java
@@ -399,8 +399,10 @@ public class RangeDopplerGeocodingOp extends Operator {
             }
         } else {
             if (applyRadiometricNormalization && mission.contains("ERS")) {
-                throw new OperatorException("For radiometric normalization of ERS product, please use 'Remove Antenna" +
-                        " Pattern' first to remove calibration factors applied and to apply ADC correction");
+                throw new OperatorException("For radiometric normalization of ERS product, please use 'Remove" +
+                        " Antenna Pattern' operator to remove calibration factors applied and to apply ADC, or" +
+                        " use one of the following user graphs: " +
+                        " 'RemoveAntPat_Orthorectify' or 'RemoveAntPat_Multilook_Orthorectify'.");
             }
         }
     }
