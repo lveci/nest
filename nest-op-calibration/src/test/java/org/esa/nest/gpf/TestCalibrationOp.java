efc12fbc29f80249b269b2c3df9f444d9f740b79
lveci <lveci>
2010-03-01 21:53:25 +0000

    *** empty log message ***

--- nest-op-calibration/src/test/java/org/esa/nest/gpf/TestCalibrationOp.java
+++ nest-op-calibration/src/test/java/org/esa/nest/gpf/TestCalibrationOp.java
@@ -25,7 +25,7 @@ public class TestCalibrationOp extends TestCase {
     private final static String inputPathIMS =     TestUtils.rootPathExpectedProducts+"\\input\\subset_0_of_ERS-2_SAR_SLC-ORBIT_10249_DATE__06-APR-1997_03_09_34.dim";
     private final static String expectedPathIMS =  TestUtils.rootPathExpectedProducts+"\\expected\\subset_0_of_ERS-2_SAR_SLC-ORBIT_10249_DATE__06-APR-1997_03_09_34_Calib.dim";
 
-    private String[] productTypeExemptions = { "_BP", "XCA", "WVW", "WVI", "WVS", "WSS" };
+    private String[] productTypeExemptions = { "_BP", "XCA", "WVW", "WVI", "WVS", "WSS", "DOR" };
 
     @Override
     protected void setUp() throws Exception {
