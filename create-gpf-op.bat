c8f7e1760517fa0600426d08cdec72b0ea83b99b
lveci <lveci>
2010-04-02 00:38:18 +0000

    3C to 4A

--- create-gpf-op.bat
+++ create-gpf-op.bat
@@ -1,7 +1,7 @@
 mvn archetype:generate ^
  -DarchetypeGroupId=org.esa.nest.maven ^
  -DarchetypeArtifactId=maven-nest-gpf-archetype ^
- -DarchetypeVersion=3C ^
+ -DarchetypeVersion=4A ^
  -DgroupId=org.esa.nest ^
  -DartifactId=%1 ^
- -Dversion=3C
+ -Dversion=4A
