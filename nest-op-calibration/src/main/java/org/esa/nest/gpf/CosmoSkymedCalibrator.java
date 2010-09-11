1daf77fd71a2fca9c91a86434587711aa2ebf423
lveci <lveci>
2010-09-11 00:02:18 +0000

    *** empty log message ***

--- nest-op-calibration/src/main/java/org/esa/nest/gpf/CosmoSkymedCalibrator.java
+++ nest-op-calibration/src/main/java/org/esa/nest/gpf/CosmoSkymedCalibrator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2002-2010 by ?
+ * Copyright (C) 2002-2010 by Jason Fritz
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
