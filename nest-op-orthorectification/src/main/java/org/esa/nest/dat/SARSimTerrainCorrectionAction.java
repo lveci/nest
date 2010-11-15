ca72b7755adb60b9008fffdb9b1f5fc5a28d50a7
junlu <junlu>
2010-11-15 18:52:20 +0000

    modified to use single graph

--- nest-op-orthorectification/src/main/java/org/esa/nest/dat/SARSimTerrainCorrectionAction.java
+++ nest-op-orthorectification/src/main/java/org/esa/nest/dat/SARSimTerrainCorrectionAction.java
@@ -18,15 +18,23 @@ package org.esa.nest.dat;
 import org.esa.beam.framework.ui.command.CommandEvent;
 import org.esa.beam.visat.actions.AbstractVisatAction;
 import org.esa.nest.gpf.SARSimTerrainCorrectionOp;
+import org.esa.nest.dat.plugins.graphbuilder.GraphBuilderDialog;
+
+import java.io.File;
 
 public class SARSimTerrainCorrectionAction extends AbstractVisatAction {
 
     @Override
     public void actionPerformed(final CommandEvent event) {
-        SARSimTerrainCorrectionDialog dialog = new SARSimTerrainCorrectionDialog(getAppContext(),
-                "SAR Sim Terrain Correction", "SARSimGeocodingOp");
-        dialog.setTargetProductNameSuffix(SARSimTerrainCorrectionOp.PRODUCT_SUFFIX);
+
+        final GraphBuilderDialog dialog = new GraphBuilderDialog(getAppContext(),
+                "SAR Sim Terrain Correction", getHelpId(), false);
         dialog.show();
+
+        final File graphPath = GraphBuilderDialog.getInternalGraphFolder();
+        final File graphFile =  new File(graphPath, "SARSimTCGraph.xml");
+
+        dialog.LoadGraph(graphFile);
     }
 
 }
