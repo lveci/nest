6f949e5a8257cfd6110851020052efa6255d7f10
lveci <lveci>
2010-11-22 22:18:11 +0000

    *** empty log message ***

--- nest-reader-ers-ceos/src/main/java/org/esa/nest/dataio/ceos/CeosDB.java
+++ nest-reader-ers-ceos/src/main/java/org/esa/nest/dataio/ceos/CeosDB.java
@@ -202,7 +202,7 @@ public final class CeosDB {
             }
 
         } catch(Exception e) {
-            System.out.println(" " +e.toString() + " for "+ name);
+            System.out.println(" " +e.toString() +":"+e.getCause().toString() + " for "+ name);
 
             //throw new IllegalBinaryFormatException(e.toString(), reader.getCurrentPos());
         }
