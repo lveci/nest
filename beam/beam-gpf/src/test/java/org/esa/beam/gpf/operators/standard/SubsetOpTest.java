bc47fe5bb70f678967f010d326a840f9cef615d3
lveci <lveci>
2011-01-21 22:10:44 +0000

    beam update

--- beam/beam-gpf/src/test/java/org/esa/beam/gpf/operators/standard/SubsetOpTest.java
+++ beam/beam-gpf/src/test/java/org/esa/beam/gpf/operators/standard/SubsetOpTest.java
@@ -15,14 +15,35 @@
  */
 package org.esa.beam.gpf.operators.standard;
 
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
+import com.vividsolutions.jts.geom.Polygon;
 import junit.framework.TestCase;
 import org.esa.beam.framework.datamodel.Band;
+import org.esa.beam.framework.datamodel.CrsGeoCoding;
+import org.esa.beam.framework.datamodel.GcpDescriptor;
+import org.esa.beam.framework.datamodel.GcpGeoCoding;
+import org.esa.beam.framework.datamodel.GeoPos;
 import org.esa.beam.framework.datamodel.MetadataAttribute;
 import org.esa.beam.framework.datamodel.MetadataElement;
+import org.esa.beam.framework.datamodel.PixelPos;
+import org.esa.beam.framework.datamodel.Placemark;
 import org.esa.beam.framework.datamodel.Product;
 import org.esa.beam.framework.datamodel.ProductData;
+import org.esa.beam.framework.dataop.maptransf.Datum;
+import org.esa.beam.framework.gpf.GPF;
+import org.esa.beam.framework.gpf.graph.GraphException;
+import org.geotools.referencing.crs.DefaultGeographicCRS;
+import org.junit.Test;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.operation.TransformException;
 
+import java.awt.Rectangle;
+import java.awt.geom.AffineTransform;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class SubsetOpTest extends TestCase {
 
@@ -46,6 +67,118 @@ public class SubsetOpTest extends TestCase {
         assertNull(tp.getBand("radiance_2"));
         assertNotNull(tp.getBand("radiance_3"));
     }
+
+    public void testInstantiationWithGPF() throws GraphException {
+        GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis();
+        GeometryFactory gf = new GeometryFactory();
+        Polygon polygon = gf.createPolygon(gf.createLinearRing(new Coordinate[]{
+                new Coordinate(-5, 5),
+                new Coordinate(5, 5),
+                new Coordinate(5, -5),
+                new Coordinate(-5, -5),
+                new Coordinate(-5, 5),
+        }), null);
+
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("geoRegion", polygon.toText());
+        parameters.put("fullSwath", true);
+        final Product sp = createTestProduct(100, 100);
+        assertNotNull(sp.getGeoCoding());
+
+        Product tp = GPF.createProduct("Subset", parameters, sp);
+        assertNotNull(tp);
+        assertEquals(100, tp.getSceneRasterWidth());
+        assertEquals(51, tp.getSceneRasterHeight());
+    }
+
+
+    public void testGeometry() throws Exception {
+        final GeometryFactory gf = new GeometryFactory();
+        final Product sp = createTestProduct(100, 100);
+        assertNotNull(sp.getGeoCoding());
+
+        final Polygon roi = gf.createPolygon(gf.createLinearRing(new Coordinate[]{
+                new Coordinate(-5, 5),
+                new Coordinate(5, 5),
+                new Coordinate(5, -5),
+                new Coordinate(-5, -5),
+                new Coordinate(-5, 5),
+        }), null);
+
+        final SubsetOp op = new SubsetOp();
+        op.setSourceProduct(sp);
+        op.setBandNames(new String[]{"radiance_1", "radiance_3"});
+        op.setGeoRegion(roi);
+
+        assertEquals(roi, op.getGeoRegion());
+
+        final Product tp = op.getTargetProduct();
+        assertNotNull(tp);
+        assertEquals(new Rectangle(25, 25, 51, 51), op.getRegion());
+        assertEquals(51, tp.getSceneRasterWidth());
+        assertEquals(51, tp.getSceneRasterHeight());
+        assertNotNull(tp.getBand("radiance_1"));
+        assertNotNull(tp.getBand("radiance_3"));
+    }
+
+    @Test
+    public void testComputationOfProductGeometriesAndPixelRegions() throws TransformException, FactoryException {
+        Geometry geometry;
+
+        Product product = new Product("N", "T", 360, 180);
+        AffineTransform at = AffineTransform.getTranslateInstance(-180, -90);
+        CrsGeoCoding geoCoding = new CrsGeoCoding(DefaultGeographicCRS.WGS84, new Rectangle(360, 180), at);
+        product.setGeoCoding(geoCoding);
+        geometry = SubsetOp.computeProductGeometry(product);
+        assertTrue(geometry instanceof Polygon);
+        assertEquals("POLYGON ((-179.5 -89.5, -179.5 89.5, 179.5 89.5, 179.5 -89.5, -179.5 -89.5))", geometry.toString());
+
+        Rectangle rectangle;
+
+        rectangle = SubsetOp.computePixelRegion(product, geometry, 0);
+        assertEquals(new Rectangle(360, 180), rectangle);
+
+        SubsetOp op = new SubsetOp();
+        op.setSourceProduct(product);
+        op.setRegion(new Rectangle(180 - 50, 90 - 25, 100, 50));
+        product = op.getTargetProduct();
+        geometry = SubsetOp.computeProductGeometry(product);
+        assertTrue(geometry instanceof Polygon);
+        assertEquals("POLYGON ((-49.5 -24.5, -49.5 24.5, 49.5 24.5, 49.5 -24.5, -49.5 -24.5))", geometry.toString());
+
+        // BBOX fully contained, with border=0
+        rectangle = SubsetOp.computePixelRegion(product, createBBOX(0.0, 0.0, 10.0, 10.0), 0);
+        assertEquals(new Rectangle(50, 25, 11, 11), rectangle);
+
+        // BBOX fully contained, with border=1
+        rectangle = SubsetOp.computePixelRegion(product, createBBOX(0.0, 0.0, 10.0, 10.0), 1);
+        assertEquals(new Rectangle(49, 24, 13, 13), rectangle);
+
+        // BBOX intersects product rect in upper left
+        rectangle = SubsetOp.computePixelRegion(product, createBBOX(45.5, 20.5, 100.0, 50.0), 0);
+        assertEquals(new Rectangle(95, 45, 5, 5), rectangle);
+
+        // Product bounds fully contained in BBOX
+        rectangle = SubsetOp.computePixelRegion(product, createBBOX(-180, -90, 360, 180), 0);
+        assertEquals(new Rectangle(0, 0, 100, 50), rectangle);
+
+        // BBOX not contained
+        rectangle = SubsetOp.computePixelRegion(product, createBBOX(60.0, 0.0, 10.0, 10.0), 0);
+        assertEquals(true, rectangle.isEmpty());
+    }
+
+    private static Polygon createBBOX(double x, double y, double w, double h) {
+        GeometryFactory factory = new GeometryFactory();
+        final LinearRing ring = factory.createLinearRing(new Coordinate[]{
+                new Coordinate(x, y),
+                new Coordinate(x + w, y),
+                new Coordinate(x + w, y + h),
+                new Coordinate(x, y + h),
+                new Coordinate(x, y)
+        });
+        return factory.createPolygon(ring, null);
+    }
+
 
     public void testCopyMetadata() throws Exception {
         final Product sp = createTestProduct(100, 100);
@@ -99,24 +232,36 @@ public class SubsetOpTest extends TestCase {
     }
 
     private Product createTestProduct(int w, int h) {
-        Product testProduct = new Product("p", "t", w, h);
+        Product product = new Product("p", "t", w, h);
 
-        Band band1 = testProduct.addBand("radiance_1", ProductData.TYPE_INT32);
+        Placemark[] gcps = {
+                new Placemark("p1", "p1", "", new PixelPos(0.5f, 0.5f), new GeoPos(10, -10), GcpDescriptor.INSTANCE,
+                              null),
+                new Placemark("p2", "p2", "", new PixelPos(w - 0.5f, 0.5f), new GeoPos(10, 10), GcpDescriptor.INSTANCE,
+                              null),
+                new Placemark("p3", "p3", "", new PixelPos(w - 0.5f, h - 0.5f), new GeoPos(-10, 10),
+                              GcpDescriptor.INSTANCE, null),
+                new Placemark("p4", "p4", "", new PixelPos(0.5f, h - 0.5f), new GeoPos(-10, -10),
+                              GcpDescriptor.INSTANCE, null),
+        };
+        product.setGeoCoding(new GcpGeoCoding(GcpGeoCoding.Method.POLYNOMIAL1, gcps, w, h, Datum.WGS_84));
+
+        Band band1 = product.addBand("radiance_1", ProductData.TYPE_INT32);
         int[] intValues = new int[w * h];
         Arrays.fill(intValues, 1);
         band1.setData(ProductData.createInstance(intValues));
 
-        Band band2 = testProduct.addBand("radiance_2", ProductData.TYPE_FLOAT32);
+        Band band2 = product.addBand("radiance_2", ProductData.TYPE_FLOAT32);
         float[] floatValues = new float[w * h];
         Arrays.fill(floatValues, 2.5f);
         band2.setData(ProductData.createInstance(floatValues));
 
-        Band band3 = testProduct.addBand("radiance_3", ProductData.TYPE_INT16);
+        Band band3 = product.addBand("radiance_3", ProductData.TYPE_INT16);
         band3.setScalingFactor(0.5);
         short[] shortValues = new short[w * h];
         Arrays.fill(shortValues, (short) 6);
         band3.setData(ProductData.createInstance(shortValues));
 
-        return testProduct;
+        return product;
     }
 }
