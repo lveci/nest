bb5abfcbb79479939f4dbf273797beaf788c3b05
lveci <lveci>
2011-03-28 19:42:53 +0000

    fix subsetOp

--- nest-graph-builder/src/main/java/org/esa/nest/gpf/SubsetOp.java
+++ nest-graph-builder/src/main/java/org/esa/nest/gpf/SubsetOp.java
@@ -16,6 +16,7 @@
 package org.esa.nest.gpf;
 
 import com.bc.ceres.core.ProgressMonitor;
+import com.vividsolutions.jts.geom.Geometry;
 import org.esa.beam.framework.dataio.ProductReader;
 import org.esa.beam.framework.dataio.ProductSubsetBuilder;
 import org.esa.beam.framework.dataio.ProductSubsetDef;
@@ -31,12 +32,13 @@ import org.esa.beam.framework.gpf.annotations.OperatorMetadata;
 import org.esa.beam.framework.gpf.annotations.Parameter;
 import org.esa.beam.framework.gpf.annotations.SourceProduct;
 import org.esa.beam.framework.gpf.annotations.TargetProduct;
+import org.esa.beam.util.converters.JtsGeometryConverter;
 
 import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@OperatorMetadata(alias = "Subset",
+@OperatorMetadata(alias = "SubsetOp",
         category = "Utilities",
         description = "Create a spatial subset of the source product.")
 public class SubsetOp extends Operator {
@@ -64,6 +66,12 @@ public class SubsetOp extends Operator {
     @Parameter(defaultValue = "1")
     private int subSamplingY = 1;
 
+    @Parameter(converter = JtsGeometryConverter.class,
+               description = "WKT-format, " +
+                       "e.g. POLYGON((<lon1> <lat1>, <lon2> <lat2>, ..., <lon1> <lat1>))\n" +
+                       "(make sure to quote the option due to spaces in <geometry>)")
+    private Geometry geoRegion;
+
     private ProductReader subsetReader = null;
     private final Map<Band, Band> bandMap = new HashMap<Band, Band>();
 
@@ -88,6 +96,16 @@ public class SubsetOp extends Operator {
             subsetDef.addNodeNames(sourceProduct.getBandNames());
         }
         subsetDef.setRegion(regionX, regionY, width, height);
+
+        if (geoRegion != null) {
+            final Rectangle region = org.esa.beam.gpf.operators.standard.SubsetOp.computePixelRegion(sourceProduct, geoRegion, 0);
+            if (region != null) {
+                if (region.isEmpty()) {
+                    System.out.println("Subset: No intersection with source product boundary "+sourceProduct.getName());
+                }
+                subsetDef.setRegion(region);
+            }
+        }
 
         subsetDef.setSubSampling(subSamplingX, subSamplingY);
         subsetDef.setIgnoreMetadata(false);
