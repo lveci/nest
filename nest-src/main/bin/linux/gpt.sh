17decd821231643ffe309298bcbdd440fffa16df
lveci <lveci>
2011-04-01 19:45:53 +0000

    *** empty log message ***

--- nest-src/main/bin/linux/gpt.sh
+++ nest-src/main/bin/linux/gpt.sh
@@ -1,5 +1,16 @@
 #! /bin/sh
 
+if [ -z "$NEST_HOME" ]; then
+    echo
+    echo Error: NEST_HOME not found in your environment.
+    echo Please set the NEST_HOME variable in your environment to match the
+    echo location of the NEST installation
+    echo
+    exit 2
+fi
+
+chmod 755 $NEST_HOME/jre/bin/*
+
 $NEST_HOME/jre/bin/java \
     -Xmx1280M -XX:CompileThreshold=100 -Xverify:none \
     -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xconcurrentio \
